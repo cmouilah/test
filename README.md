1b53045cda2d2998afb10d1bb1bda0729baa9197
Mouilah <cmouilah@gmail.com>
2025-11-13 11:31:48 +0100

    *changement*

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# test
+# ma premiere modification
 ce document a pour rôle d'apprendre à utiliser le gihub
---
+++ code.py
@@ -0,0 +1,11 @@
+# Programme simple : calcul de la somme de deux nombres
+
+# Demande à l'utilisateur d'entrer deux nombres
+a = float(input("Entrez le premier nombre : "))
+b = float(input("Entrez le deuxième nombre : "))
+
+# Calcule la somme
+somme = a + b
+
+# Affiche le résultat
+print("La somme de", a, "et", b, "est :", somme)
